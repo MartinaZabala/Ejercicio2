0f6d1d34c7c02eac2e887e6a0990c56878d5b772
MartinaZabala <martina.zabala@alumnos.frm.utn.edu.ar>
2023-09-02 11:48:57 -0300

    Ejercicio 02

---
+++ src/main/java/com/utn/ejer02/entidades/Persona.java
@@ -0,0 +1,32 @@
+package com.utn.ejer02.entidades;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.boot.registry.selector.spi.StrategyCreator;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+import java.io.Serializable;
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Persona implements Serializable {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private int edad;
+
+    // RELACION ONE TO ONE UNIDIRECCIONAL
+    // El Cascadeo propaga las operaciones y orphanRemoval asegura que se elimine la relacionada
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "domicilio_id")
+    private Domicilio domicilio;
+
+
+}
